ca2a389018744947042b24c9e240d62bdd01e1af
Abidur Rahman Arko <aroarko.sd@gmail.com>
2024-11-08 23:52:24 +0800

    problem 4

---
+++ src/problem-4.ts
@@ -0,0 +1,40 @@
+{
+  // problem 4
+
+  type Circle = {
+    shape: "circle";
+    radius: number;
+  };
+
+  type Rectangle = {
+    shape: "rectangle";
+    width: number;
+    height: number;
+  };
+
+  type Shape = Circle | Rectangle;
+
+  function calculateShapeArea(inputShape: Shape): number {
+    if (inputShape.shape === "circle") {
+      const result = (Math.PI * inputShape.radius * inputShape.radius).toFixed(
+        2
+      );
+      return parseFloat(result);
+    } else if (inputShape.shape === "rectangle") {
+      const result = (inputShape.height * inputShape.width).toFixed(2);
+      return parseFloat(result);
+    } else {
+      return 0;
+    }
+  }
+
+  const circleArea = calculateShapeArea({ shape: "circle", radius: 5 });
+  console.log(circleArea);
+
+  const rectangleArea = calculateShapeArea({
+    shape: "rectangle",
+    width: 4,
+    height: 6,
+  });
+  console.log(rectangleArea);
+}
